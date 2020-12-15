7d3fb46564757f740f2759e0f5e65cd20e244b4b
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-15 12:21:02 +0000

    Added answer

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import numpy as np
 def genstate( N, index ) : 
   spins = N*[0]
   # Your code goes here
-  
+  for i in range(N) :  
+      spins[i] = np.floor( index / 2**(N-1-i) )
+      index = index - spins[i]*(2**(N-1-i))
+      if spins[i]==0 : spins[i] = -1
   return spins
   
 nspins = 3
